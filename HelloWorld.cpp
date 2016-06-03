8dbf32adfb91d1c9a348bc8c1638185de52a7abd
Arnóbio Duarte <arnobiojunior2010@gmail.com>
2016-06-03 21:43:26 +0000

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,10 +1,9 @@
-#include <iostream> //integrando a biblioteca
+#include <iostream> //Estas duas primeiras linhas do código, fazem com que as bibliotecas com entrada e saída do terminal estejam disponíveis para o programa.
+using namespace std;
 
-using namespace std; //
-
-int main()  //Função Principal
+int main() //Esta linha diz que main é uma função sem parâmetros que ao terminar sua execução retornará um valor inteiro int.
 { //Abrindo Função principal
 	
-	cout << "HelloWorld!" << endl; //Imprimindo na tela
-	return 0; //
+	cout << "HelloWorld!" << endl; //Faz com que o texto entre aspas seja exibido na tela.
+	return 0; //Este comando termina a invocação da função main e fornece 0 como o valor da função. O programa termina quando este comando é executado.
 } //Fechando função principal
