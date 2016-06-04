1fb0383bd4028aaa385ada813a5bddd3b2d45775
Arnóbio Duarte <arnobiojunior2010@gmail.com>
2016-06-04 02:58:12 +0000

    O seguinte programa tem com principal intento mostrar funções básicas da linguagem de programação C++.

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() //Esta linha diz que main é uma função sem parâmetros que ao terminar sua execução retornará um valor inteiro int.
-{ //Abrindo Função principal
+{ //Abrindo Função principal.
 	
 	cout << "HelloWorld!" << endl; //Faz com que o texto entre aspas seja exibido na tela.
 	return 0; //Este comando termina a invocação da função main e fornece 0 como o valor da função. O programa termina quando este comando é executado.
