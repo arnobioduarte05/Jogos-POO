25cbbea48905259d9ee0bbb3726a1a94c1587ece
Arnóbio Duarte <arnobiojunior2010@gmail.com>
2016-06-04 02:30:27 +0000

    Com o comentário.
    Com o comentário.

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -6,4 +6,4 @@ int main() //Esta linha diz que main é uma função sem parâmetros que ao term
 	
 	cout << "HelloWorld!" << endl; //Faz com que o texto entre aspas seja exibido na tela.
 	return 0; //Este comando termina a invocação da função main e fornece 0 como o valor da função. O programa termina quando este comando é executado.
-} //Fechando função principal
+} //Fechando função principal.
