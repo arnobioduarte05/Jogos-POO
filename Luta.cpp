46bcd508f56a48eb3d356d08a54e28353ec1fa24
Arnóbio Duarte <arnobiojunior2010@gmail.com>
2016-06-06 20:51:44 +0000

    Com o comentário

--- Luta.cpp
+++ Luta.cpp
@@ -1,5 +1,5 @@
 #include <iostream>//Faz com que as bibliotecas com entrada e saída de terminal estejam disponíveis para o programa.
-#include <string>
+#include <string> //Adcionando a biblioteca.
 #include <ctime> //Integrando a biblioteca que vai usar o tempo como "semente" do gerador.
 using namespace std;//Faz com que as bibliotecas com entrada e saída de terminal estejam disponíveis para o programa.
 
@@ -19,16 +19,13 @@ int main()//Função princcipal
 
 	while (sair == false) //Quando o while é executado, a expressão booleana é avaliada. Neste caso, se avaliado como false, o loop nunca é executado. 
 	{
-		/*Imprime o dano causado*/
-		cout << "-" << danoc1 << "                           -" << danoc2 << endl; 
+		cout << "-" << danoc1 << "                           -" << danoc2 << endl; 	//Imprime o dano causado
 		
-		/*Imprimindo os personagens.*/
-		cout << " o                             o" << endl; 
-		cout << ".T./                         \\.T. " << endl;
-		cout << " ^                             ^" << endl;
+		cout << " o                             o" << endl; 	//Imprimindo os personagens.
+		cout << ".T./                         \\.T. " << endl;	//Imprimindo os personagens.
+		cout << " ^                             ^" << endl;	//Imprimindo os personagens.
 		
-		/*Imprime o status do personagem*/
-		cout << "Atq: 4                        Atq: 4" << endl;
+		cout << "Atq: 4                        Atq: 4" << endl;	//Imprime o status do personagem
 		cout << "Def: 2                        Def: 2" << endl;
 		
 		cout << jog1 << "                         " << jog2 << endl; //Faz a impressão na tela dos nomes dos jogadores.
@@ -42,43 +39,42 @@ int main()//Função princcipal
 		danoc1 = dado6 - 4; //Faz cálculo do dano causado ao lutaodr 1
 	
 		if (danoc1 < 0) //Se o dano causado for menor que 0, entrará nessa condição
-		{ 
+		{ //abrindo a condição
 			danoc1 = 0; //Se entrou na condição, dano causado igual a 0.
-		}
+		}//fechando a condição
 		vida1 = vida1 - danoc1;//Causando dano ao lutador 1.
 		
 		dado6 = rand() % 6 + 1; //Gerando número aleatório
 		danoc2 = dado6 - 4; //Cálculo do dano causado ao lutador 2
 		
 		if (danoc2 < 0) //Se o dano causado for menor que 0, entrará nesta condição
-		{ 
+		{ //abrindo a condição
 			danoc2 = 0; //Se entrou na condiçao, dano causado é 0
-		} 
+		} //fechando a condição
 		vida2 = vida2 - danoc2;//Causando dano ao lutador 2.
 		
 		system("cls"); //Limpa a tela
      /*-------------------------------------------------------------------------------------*/
-		/*Se a vida de ambos chegarem a menor ou igual a 0, os dois perderão */
-		if (vida1 <= 0 || vida2 <= 0) 
-		{ 
-			sair = true; 
-		}
+		if (vida1 <= 0 || vida2 <= 0) //Se a vida de ambos chegarem a menor ou igual a 0, os dois perderão
+		{ //abrindo a condição
+			sair = true; //o jogo acabará.
+		}//fechando a condição
 	}
      /*-----------------------------------------------------------------------------------*/
 	if (vida1 < vida2) //Condição mostrada se o jogador 2 for vencedor
 	{ //Abrindo a condição
-		cout << jog2 << " Venceu!" << endl;
+		cout << jog2 << " Venceu!" << endl;//Imprime quando o jogador 2 vencer.
 	} //Fechando a condição
     /*-------------------------------------------------------------------------------------*/
 	else if (vida2 < vida1) //Condição mostrada se o jogador 1 for o vencedor
-	{ 
+	{ //abrindo a condição else if
 		cout << jog1 << " Venceu!" << endl;
-	}
+	}//fechando a condição else if
     /*---------------------------------------------------------------------------------------*/
 	else //Se o life dos jogadores forem semelhantes, entrará nesta condição
-	{ 
+	{ //abrindo else
 		cout << "Empate!" << endl;
-	} 
+	} //fechando o else
      /*--------------------------------------------------------------------------------------*/
 	return 0;//Este comando termina a invocação da função main e fornece 0 como valor de função. O programa termina quando este comando for executado.
 } 
